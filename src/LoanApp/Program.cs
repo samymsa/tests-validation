597d3b4f2ed4767f4fa7edb8a8724e2935b174ba
Samy MOSA <samymosa02@gmail.com>
2024-03-26 11:25:28 +0100

    feat: :sparkles: Add CsvAmortizationScheduleWriter

--- src/LoanApp/Program.cs
+++ src/LoanApp/Program.cs
@@ -1,4 +1,10 @@
-﻿using LoanApp.LoanParser;
+﻿using LoanApp.AmortizationScheduleWriter;
+using LoanApp.InterestRateStrategy;
+using LoanApp.Loan;
+using LoanApp.LoanParser;
+using LoanApp.MonthlyPaymentCalculator;
+
+const string OUTPUT_FILE = "amortization.csv";
 
 MortgageParser parser = new();
 int exitCode = parser.ParseArgs(args);
@@ -6,4 +12,16 @@ if (exitCode != 0)
 {
     return exitCode;
 }
+decimal principal = parser.Principal ?? throw new ArgumentException("Principal must not be null");
+int term = parser.Term ?? throw new ArgumentException("Term must not be null");
+decimal rate = parser.Rate ?? throw new ArgumentException("Rate must not be null");
+
+Mortgage mortgage = new(principal, term, new FixedInterestRate(rate));
+FixedMonthlyPaymentCalculator calculator = new();
+using (TextWriter writer = new StreamWriter(OUTPUT_FILE))
+{
+    CsvAmortizationScheduleWriter amortizationScheduleWriter = new(writer, calculator);
+    amortizationScheduleWriter.WriteAmortizationSchedule(mortgage);
+}
+
 return 0;
