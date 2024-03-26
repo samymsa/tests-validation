643d562507af9212b940baafbefb515392208afe
Samy MOSA <samymosa02@gmail.com>
2024-03-26 09:54:05 +0100

    feat: :sparkles: Add BasicMonthlyPaymentCalculator

---
+++ src/LoanApp/MonthlyPaymentCalculator/BasicMonthlyPaymentCalculator.cs
@@ -0,0 +1,16 @@
+using LoanApp.Loan;
+
+namespace LoanApp.MonthlyPaymentCalculator;
+
+public class BasicMonthlyPaymentCalculator : IMonthlyPaymentCalculator
+{
+    public decimal CalculateMonthlyPayment(ILoan loan)
+    {
+        decimal monthlyRate = loan.Rate / 12 / 100;
+        if (monthlyRate == 0)
+        {
+            return loan.Principal / loan.Term;
+        }
+        return loan.Principal * monthlyRate / (1 - (decimal)Math.Pow(1 + (double)monthlyRate, -loan.Term));
+    }
+}
---
+++ tests/LoanApp.Tests/MonthlyPaymentCalculator/BasicMonthlyPaymentCalculatorTests.cs
@@ -0,0 +1,23 @@
+namespace LoanApp.Tests.MonthlyPaymentCalculator;
+
+using LoanApp.InterestRateStrategy;
+using LoanApp.Loan;
+using LoanApp.MonthlyPaymentCalculator;
+
+public class BasicMonthlyPaymentCalculatorTests
+{
+    public static IEnumerable<object[]> Data =>
+    [
+        [new Mortgage(200000, 15 * 12, new FixedInterestRate(3.9m)), 1469.37m],
+        [new Mortgage(200000, 15 * 12, new FixedInterestRate(0.0m)), 1111.11m],
+    ];
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void CalculateMonthlyPayment_ReturnsExpectedValue(ILoan loan, decimal expected)
+    {
+        BasicMonthlyPaymentCalculator calculator = new();
+        decimal actual = calculator.CalculateMonthlyPayment(loan);
+        Assert.Equal(expected, actual, 2);
+    }
+}
